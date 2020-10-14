217dfc42063a483da292080f52cd8488a1c8b9ac
Sakib Ahmad Siddiquee <sakib15-10776@diu.edu.bd>
2020-10-15 02:13:37 +0600

    edit button added and coding

--- enroll/views.py
+++ enroll/views.py
@@ -14,6 +14,18 @@ def add_show(request):
     stud = User.objects.all()
     return render(request, 'enroll/addandshow.html',{'form':fm, 'stu':stud})
 
+def update_data(request,id):
+    if request.method == 'POST':
+        pi = User.objects.get(pk=id)
+        fm = StudentRegistration(request.POST, instance=pi)
+        if fm.is_valid():
+            fm.save()
+    else:
+        pi = User.objects.get(pk=id)
+        fm = StudentRegistration(instance=pi)
+    return render(request, 'enroll/updatestudent.html',{'form':fm})
+
+
 def delete_data(request, id):
     if request.method == 'POST':
         pi = User.objects.get(pk=id)
